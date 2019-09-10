c2a47ea42bd8c6a070a66a0d668b5916ec99fefe
sw-hw <powerofjesus@mail.ru>
2019-09-10 09:42:28 +0300

    Remove task for ILI9488

--- Software/Firmware/Src/freertos.c
+++ Software/Firmware/Src/freertos.c
@@ -65,10 +65,8 @@ typedef struct {
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
 __IO EncoderRotate_t EncoderRotate = ENCODER_ROTATE_NO;
-__IO ADC_Signals_t ADC_Signals;
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
-osThreadId TaskILI9488Handle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -78,7 +76,6 @@ int16_t ConvertTo_dB(int16_t value);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void const * argument);
-void StartTaskILI9488(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -98,6 +95,7 @@ void HAL_ADCEx_InjectedConvCpltCallback(ADC_HandleTypeDef* hadc)
 	static float avg_left  = ADC_CONST_OFFSET;
 	static float avg_right = ADC_CONST_OFFSET;
 	static uint16_t vu_counter = 0;
+	static ADC_Signals_t ADC_Signals;
 	int16_t signal_r_abs, signal_l_abs;
 	UBaseType_t uxSavedInterruptStatus = taskENTER_CRITICAL_FROM_ISR();
 	ADC_Signals.last_left = (int16_t)HAL_ADCEx_InjectedGetValue(hadc, ADC_INJECTED_RANK_1);
@@ -122,6 +120,7 @@ void HAL_ADCEx_InjectedConvCpltCallback(ADC_HandleTypeDef* hadc)
 		ADC_Signals.signal_right_db = ConvertTo_dB(ADC_Signals.max_right);
 		ADC_Signals.max_right = 0;
 	}
+	VU_DisplaySignal(ADC_Signals.signal_left_db, ADC_Signals.signal_right_db);
 	taskEXIT_CRITICAL_FROM_ISR(uxSavedInterruptStatus);
 }
 
@@ -175,10 +174,6 @@ void MX_FREERTOS_Init(void) {
   osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
-  /* definition and creation of TaskILI9488 */
-  osThreadDef(TaskILI9488, StartTaskILI9488, osPriorityLow, 0, 128);
-  TaskILI9488Handle = osThreadCreate(osThread(TaskILI9488), NULL);
-
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -194,7 +189,6 @@ void MX_FREERTOS_Init(void) {
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
-    
     
     
 
@@ -214,28 +208,6 @@ void StartDefaultTask(void const * argument)
 	osDelay(50);
   }
   /* USER CODE END StartDefaultTask */
-}
-
-/* USER CODE BEGIN Header_StartTaskILI9488 */
-/**
-* @brief Function implementing the TaskILI9488 thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartTaskILI9488 */
-void StartTaskILI9488(void const * argument)
-{
-  /* USER CODE BEGIN StartTaskILI9488 */
-  /* Infinite loop */
-  for(;;)
-  {
-	  taskENTER_CRITICAL();
-	  if(TDA7439_GetAmplifierState())
-		  VU_DisplaySignal(ADC_Signals.signal_left_db, ADC_Signals.signal_right_db);
-	  taskEXIT_CRITICAL();
-	  //osDelay(1);
-  }
-  /* USER CODE END StartTaskILI9488 */
 }
 
 /* Private application code --------------------------------------------------*/
